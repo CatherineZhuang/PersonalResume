e6ed1bdae6d2997f08236fefe548ad6c4b59b167
CatherineZhuang <catherinezhuzhuang@gmail.com>
2019-01-13 14:56:06 -0500

    Updates

---
+++ precache-manifest.8185d28fdf2cd448eac5a485838fe6b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "339300c993b229d8b358",
+    "url": "/PersonalResume/static/css/main.0f8389ac.chunk.css"
+  },
+  {
+    "revision": "339300c993b229d8b358",
+    "url": "/PersonalResume/static/js/main.339300c9.chunk.js"
+  },
+  {
+    "revision": "e94bcbe8b2d685a2c424",
+    "url": "/PersonalResume/static/js/1.e94bcbe8.chunk.js"
+  },
+  {
+    "revision": "42ceee58965217f436fc",
+    "url": "/PersonalResume/static/js/runtime~main.42ceee58.js"
+  },
+  {
+    "revision": "6414ceca6806a92691b6284f567c7d6b",
+    "url": "/PersonalResume/static/media/mypic.6414ceca.JPG"
+  },
+  {
+    "revision": "4e9c817a631cc05b71ba52b807cd932f",
+    "url": "/PersonalResume/static/media/resume.4e9c817a.pdf"
+  },
+  {
+    "revision": "5f10ce44760da4ea2f0faae25b41313a",
+    "url": "/PersonalResume/index.html"
+  }
+];
